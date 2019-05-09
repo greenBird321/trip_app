b1891efb8a2f819492e3200095d9de57cf480220
lihe <1395992734@qq.com>
2019-05-09 18:38:44 +0800

    新增四个页面 以及实现banner圆角

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:trip_app/tools/imageClipper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,11 +8,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _imgUrls = [
+    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+    'https://dimg04.c-ctrip.com/images/300h0u000000j05rnD96B_C_500_280.jpg',
+    'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
+    'https://dimg03.c-ctrip.com/images/fd/tg/g1/M03/7E/19/CghzfVWw6OaACaJXABqNWv6ecpw824_C_500_280_Q90.jpg',
+  ];
+
+  final PageController _controller = PageController(
+    initialPage: 0,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557312879650&di=fe0f3fda0a456ac632fb5f744a29da1e&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F71781c2667244a2c018684636ad7cfa3073082652e6e4-Ln43kY_fw658'),
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(              // 调试需要
+                color: Colors.grey,
+                child: ClipPath(
+                  clipper: imageCliper(),
+                  child: SizedBox(
+                    height: 180,
+                    child: Swiper(
+                      itemCount: _imgUrls.length,
+                      autoplay: true,   // 自动播放
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imgUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination:new SwiperPagination(),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
---
+++ lib/tools/imageClipper.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+/// 自定义的曲线裁剪组件
+///
+class imageCliper extends CustomClipper<Path> {
+  // 得到剪辑('getClip')抽象方法, 给定被剪辑的渲染对象具有给定大小的剪辑描述
+  @override
+  Path getClip(Size size) {
+    // TODO: 自定义的主要操作内容
+    var path = Path();
+    // 使用二次贝塞尔曲线
+    // 起点变量。
+    path.lineTo(0.0, size.height - 30.0);
+    // 偏移（`Offset`）类，不可变的2D浮点偏移量。
+    // 第一个控制点和第一点变量。
+    var firstControlPoint = Offset(size.width / 4, size.height - 5.0);
+    var firstPoint = Offset(size.width / 2, size.height);
+    // 二次贝塞尔曲线（`quadraticBezierTo`）方法，
+    // 使用控制点（x1，y1）添加从当前点到给定点（x2，y2）的二次贝塞尔曲线段。
+    // 第一个中间曲线变量。
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstPoint.dx, firstPoint.dy);
+    // 第二个控制点和第二点变量。
+    var secondControlPoint =
+    Offset(size.width - (size.width / 4), size.height - 5.0);
+    // 第二个中间曲线变量。
+    var secondPoint = Offset(size.width, size.height - 30.0);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondPoint.dx, secondPoint.dy);
+    // 终点变量。
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+
+
+}
