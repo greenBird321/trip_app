f6f7a3ae788056bba2c04c004dd63ef394aa1aa9
lihe <1395992734@qq.com>
2019-05-15 10:38:17 +0800

    增加 弧形处理图片，更新Navigator

--- lib/tabBarNavigator/tab_navigator.dart
+++ lib/tabBarNavigator/tab_navigator.dart
@@ -9,8 +9,6 @@ class TabNavigator extends StatefulWidget {
   _TabNavigatorState createState() => _TabNavigatorState();
 }
 
-
-
 // 使用pageView作为BottomNavigation的Body
 class _TabNavigatorState extends State<TabNavigator> {
   final Color _kDefaultColor = Colors.grey;
@@ -35,27 +33,32 @@ class _TabNavigatorState extends State<TabNavigator> {
           MyPage(),
         ],
         onPageChanged: (pageIndex) {
-          setState(() {     // 当页面切换的时候，tabBarItem的状态也要随之切换
+          setState(() {
+            // 当页面切换的时候，tabBarItem的状态也要随之切换
             _currentIndex = pageIndex;
           });
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,    // 设置tabBar的样式 BottomNavigationBarItem 下的label是否一直显示
+        type: BottomNavigationBarType.fixed,
+        // 设置tabBar的样式 BottomNavigationBarItem 下的label是否一直显示
         currentIndex: _currentIndex,
         items: [
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                color: _kDefaultColor,
-              ),
-              activeIcon: Icon(
-                Icons.home,
-                color: _kActiveColor,
-              ),
-              title: Text('首页', style: TextStyle(color: _currentIndex != 0 ? _kDefaultColor : _kActiveColor),),
+            icon: Icon(
+              Icons.home,
+              color: _kDefaultColor,
+            ),
+            activeIcon: Icon(
+              Icons.home,
+              color: _kActiveColor,
+            ),
+            title: Text(
+              '首页',
+              style: TextStyle(
+                  color: _currentIndex != 0 ? _kDefaultColor : _kActiveColor),
+            ),
           ),
-
           BottomNavigationBarItem(
             icon: Icon(
               Icons.search,
@@ -65,9 +68,12 @@ class _TabNavigatorState extends State<TabNavigator> {
               Icons.search,
               color: _kActiveColor,
             ),
-            title: Text('搜索', style: TextStyle(color: _currentIndex != 1 ? _kDefaultColor : _kActiveColor),),
+            title: Text(
+              '搜索',
+              style: TextStyle(
+                  color: _currentIndex != 1 ? _kDefaultColor : _kActiveColor),
+            ),
           ),
-
           BottomNavigationBarItem(
             icon: Icon(
               Icons.camera_alt,
@@ -77,9 +83,12 @@ class _TabNavigatorState extends State<TabNavigator> {
               Icons.camera_alt,
               color: _kActiveColor,
             ),
-            title: Text('旅拍', style: TextStyle(color: _currentIndex != 2 ? _kDefaultColor : _kActiveColor),),
+            title: Text(
+              '旅拍',
+              style: TextStyle(
+                  color: _currentIndex != 2 ? _kDefaultColor : _kActiveColor),
+            ),
           ),
-
           BottomNavigationBarItem(
             icon: Icon(
               Icons.account_circle,
@@ -89,17 +98,21 @@ class _TabNavigatorState extends State<TabNavigator> {
               Icons.account_circle,
               color: _kActiveColor,
             ),
-            title: Text('我的', style: TextStyle(color: _currentIndex != 3 ? _kDefaultColor : _kActiveColor),),
+            title: Text(
+              '我的',
+              style: TextStyle(
+                  color: _currentIndex != 3 ? _kDefaultColor : _kActiveColor),
+            ),
           ),
         ],
         onTap: (index) {
-          _controller.jumpToPage(index);    // 通过PageView进行跳转，因为PageView是通过PageViewController管理的
+          _controller.jumpToPage(
+              index); // 通过PageView进行跳转，因为PageView是通过PageViewController管理的
           setState(() {
             _currentIndex = index;
           });
         },
       ),
-
     );
   }
 }
--- lib/tools/imageClipper.dart
+++ lib/tools/imageClipper.dart
@@ -10,11 +10,11 @@ class imageCliper extends CustomClipper<Path> {
     var path = Path();
     // 使用二次贝塞尔曲线
     // 起点变量。
-    path.lineTo(0.0, size.height - 30.0);
+    path.lineTo(0.0, size.height - 25.0);
     // 偏移（`Offset`）类，不可变的2D浮点偏移量。
     // 第一个控制点和第一点变量。
-    var firstControlPoint = Offset(size.width / 4, size.height - 5.0);
-    var firstPoint = Offset(size.width / 2, size.height);
+    var firstControlPoint = Offset(size.width / 4, size.height - 10.0);
+    var firstPoint = Offset(size.width / 2, size.height - 5 );
     // 二次贝塞尔曲线（`quadraticBezierTo`）方法，
     // 使用控制点（x1，y1）添加从当前点到给定点（x2，y2）的二次贝塞尔曲线段。
     // 第一个中间曲线变量。
@@ -22,9 +22,9 @@ class imageCliper extends CustomClipper<Path> {
         firstPoint.dx, firstPoint.dy);
     // 第二个控制点和第二点变量。
     var secondControlPoint =
-    Offset(size.width - (size.width / 4), size.height - 5.0);
+    Offset(size.width - (size.width / 4), size.height - 10.0);
     // 第二个中间曲线变量。
-    var secondPoint = Offset(size.width, size.height - 30.0);
+    var secondPoint = Offset(size.width, size.height - 25.0);
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondPoint.dx, secondPoint.dy);
     // 终点变量。
@@ -37,6 +37,4 @@ class imageCliper extends CustomClipper<Path> {
   bool shouldReclip(CustomClipper<Path> oldClipper) {
     return true;
   }
-
-
 }
